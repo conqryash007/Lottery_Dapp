1dc8fadbe408421c9c40b1dd3e8be6a202a629be
conqryash007 <guptayash200010@gmail.com>
2021-10-15 17:03:02 +0530

    changes in contract

--- LOTTERY_CONTRACT & TESTING/deploy.js
+++ LOTTERY_CONTRACT & TESTING/deploy.js
@@ -1,18 +1,28 @@
-const hdWalletProvider = require("truffle-hdwallet-provider");
 const Web3 = require("Web3");
 const { abi, bytecode } = require("./compile");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const provider = new hdWalletProvider(
-  "receive chunk sugar century school coin lyrics fluid vessel december stone forget",
-  "https://rinkeby.infura.io/v3/d74a4e3b04cc4061a37770132b6fd0b6"
-);
+
+let provider = new HDWalletProvider({
+  mnemonic: {
+    phrase: "receive chunk sugar century school coin lyrics fluid vessel december stone forget"
+  },
+  providerOrUrl: "https://rinkeby.infura.io/v3/d74a4e3b04cc4061a37770132b6fd0b6"
+});
 
 const web3 = new Web3(provider);
 
+web3.setProvider(provider);
+
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  const result = new web3.eth.Contract(abi)
+  const result = await new web3.eth.Contract(abi)
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: 1000000 });
+
+    console.log(result.options.address);
+    console.log(abi);
 };
 deploy();
+
+provider.engine.stop();
